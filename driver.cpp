593048666084e3990270ef94a7a0f4aa05b68d0a
Charles Vaughn <cvaughn@gmail.com>
2012-03-05 09:02:50 -0800

    Added rough distance calculation from geometory

--- driver.cpp
+++ driver.cpp
@@ -12,10 +12,30 @@ using namespace std;
 int vmin, vmax, smin, smax, hmin, hmax;
 
 
+// Flag set by the compiler to tell the program what mode to start in
+// DEV = developer machine with a static video
+// VSP_DEBUG = On the VSP, but displaying frames
+// VSP_RELEASE = On the VSP, no graphical output
+
+#ifndef HOST_MODE
+#define HOST_MODE DEV
+#endif
+
 void sendMessage(string msg) {
     ClientSocket *client_socket = new ClientSocket( "10.35.74.2", 5001 );
 	*client_socket << msg;
 	delete(client_socket);
+}
+
+/*
+ * Euclidean distance between two points
+ */
+double euclidDistance(Point p1, Point p2) {
+
+	double xDelta = p1.x - p2.x;
+	double yDelta = p1.y - p2.y;
+
+	return sqrt(xDelta * xDelta + yDelta * yDelta);
 }
 
 Mat findRectangles(Mat frame, bool showMask) {
@@ -39,8 +59,9 @@ Mat findRectangles(Mat frame, bool showMask) {
 	findContours(thresh, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
 
 	vector<Point> approx;
-	vector<vector<Point> > squares;
 
+	// Stores the rectangle and it's index for hierarchy checks
+	vector<vector<Point> > squares;
 	vector<int> goodRects;
 
 
@@ -57,31 +78,16 @@ Mat findRectangles(Mat frame, bool showMask) {
 		// Note: absolute value of an area is used because
 		// area may be positive or negative - in accordance with the
 		// contour orientation
-		if( approx.size() == 4 &&
-				(hierarchy[i][2] >= 0 ||
-				hierarchy[i][3] >= 0) &&
-				fabs(contourArea(Mat(approx))) > 1000 &&
+		if( approx.size() == 4 && // Only interested if there are exactly 4 points
+				(hierarchy[i][2] >= 0 || // And if it's got another rectange inside
+				hierarchy[i][3] >= 0) && // or it is inside another rectangle
+				fabs(contourArea(Mat(approx))) > 500 &&
 				isContourConvex(Mat(approx)) )
 		{
-			//double maxCosine = 0;
-
-			/*for( int j = 2; j < 5; j++ )
-			{
-				// find the maximum cosine of the angle between joint edges
-				double cosine = fabs(angle(approx[j%4], approx[j-2], approx[j-1]));
-				maxCosine = MAX(maxCosine, cosine);
-			}*/
-
-			// if cosines of all angles are small
-			// (all angles are ~90 degree) then write quandrange
-			// vertices to resultant sequence
-			//if( maxCosine < 0.3 )
 			squares.push_back(approx);
 				goodRects.push_back(i);
 		}
 	}
-
-	//cout << "Squares:: " << squares.size() << endl;
 
 	//draw loop
 	for( size_t i = 0; i < goodRects.size(); i++ )
@@ -91,23 +97,56 @@ Mat findRectangles(Mat frame, bool showMask) {
 		const Point* p = &currentRectangle[0];
 		int n = (int)currentRectangle.size();
 
+		// Red in BGR
+		Scalar color = Scalar(0,0,255);
+
+		// Is an inner rectangle
+		if (hierarchy[goodRects[i]][3] >= 0) {
+			color = Scalar(255,0,0);
+		}
+
 		//draw rectangles
-		polylines(frame, &p, &n, 1, true, Scalar(0,0,255), 2, CV_AA);
+		polylines(frame, &p, &n, 1, true, color, 2, CV_AA);
 
-		//log points of rectangles
-		//cout << Mat(squares[1])<<endl;
-
-		RotatedRect minRect;
 
 		// Check to make sure this rectangle has another inside it
         if (hierarchy[goodRects[i]][2] >= 0) {
-    		minRect = minAreaRect( Mat(currentRectangle) );
+        	/* Tell the difference between:
+        	 *
+        	 * 1-------2
+        	 * |       |
+        	 * |       |
+        	 * 0-------3
+        	 *
+        	 * 2-------3
+        	 * |       |
+        	 * |       |
+        	 * 1-------0
+        	 *
+        	 *
+        	 */
+        	double d1, d2; // The two horizontal lines
+        	if (euclidDistance(currentRectangle[0],currentRectangle[1]) > euclidDistance(currentRectangle[1],currentRectangle[2])) {
+        		d1 = euclidDistance(currentRectangle[0],currentRectangle[1]);
+        		d2 = euclidDistance(currentRectangle[2],currentRectangle[3]);
 
-        	//cout <<minRect.size.width << "x" << minRect.size.height << " " << minRect.angle << endl;
-        	double mWidth = max(minRect.size.width, minRect.size.height);
-        	double pixScale = 640.0 / mWidth;
+        		line(frame, currentRectangle[0], currentRectangle[1], Scalar(0,255,255), 3, CV_AA);
+        		line(frame, currentRectangle[2], currentRectangle[3], Scalar(0,255,255), 3, CV_AA);
+        	} else {
+        		d1 = euclidDistance(currentRectangle[1],currentRectangle[2]);
+        		d2 = euclidDistance(currentRectangle[0],currentRectangle[3]);
+        		line(frame, currentRectangle[1], currentRectangle[2], Scalar(0,255,255), 3, CV_AA);
+        		line(frame, currentRectangle[0], currentRectangle[3], Scalar(0,255,255), 3, CV_AA);
+
+        	}
+
+        	//cout << d1 << ' ' << d2 << endl;
+
+        	double midPointDistance = (d1 + d2) / 2;
+
+
         	double fovScale = 2.0 * 0.5317; // 2 * tan(56deg / 2)
-        	cout << pixScale * fovScale << " away-ish" << endl;
+        	cout << (640.0 / midPointDistance) * fovScale * 2.85 << " away-ish" << endl;
         }
 
 		int centerX = (currentRectangle[0].x + currentRectangle[1].x + currentRectangle[2].x + currentRectangle[3].x)/4;
@@ -126,19 +165,23 @@ int main(int argc, char** argv)
 {
 
 
-	vmin = 142;
-	smin = 160;
-	hmin = 64;
+	vmin = 60;
+	smin = 120;
+	hmin = 41;
 	vmax = 255;
 	smax = 255;
-	hmax = 98;
-	vmin = 130;
+	hmax = 90;
+
 	cv::VideoCapture videoCapture;
 
 	Mat frame;
 	Mat savedFrame;
 
+#if HOST_MODE == DEV
+	videoCapture.open("/home/charles/full_range.mpg");
+#else
 	videoCapture.open(0);
+#endif
 
 	cout << "Opened" << endl;
 
@@ -162,7 +205,7 @@ int main(int argc, char** argv)
     	fc++;
     	videoCapture>>frame;
 
-    	cout << "Captured " << fc << endl;
+    	//cout << "Captured " << fc << endl;
 
     	savedFrame = findRectangles(frame, (fc % 10) == 0);
 
